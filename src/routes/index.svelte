a5112ef24faa431f9c6d618a243eb898b197993f
shadofer <shadowrlrs@gmail.com>
2021-10-05 15:54:55 +0300

    Add working landing page, fix css.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,11 +2,8 @@
 	<title>Fronvo</title>
 </svelte:head>
 
-<script>
-	// Animations
-	import {fade} from 'svelte/transition';
-</script>
+<div class='center-div'>
+	<img src='../../images/fronvo-icon.webp' alt='Fronvo icon'/>
 
-<div in:fade>
-	<img class='adaptive' src='../../images/fronvo-icon.webp' alt='Fronvo icon'/>
+	<h1>Fronvo is currently under construction, come back later!</h1>
 </div>

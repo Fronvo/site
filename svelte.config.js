4e7cd498d0dacbd121b53fc25a4963ce9fbda69e
Shadofer <shadowrlrs@gmail.com>
2022-02-14 17:15:44 +0200

    Add svelte-preprocess with PostCSS support and autoprefixer.

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,6 @@
+import preprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
+
 /** @type {import('@sveltejs/kit').Config} */
 import adapter from '@sveltejs/adapter-node';
 const config = {
@@ -6,7 +9,13 @@ const config = {
 		adapter: adapter({
 					out: 'build'
 		})
-	}
+	},
+
+	preprocess: preprocess({
+		postcss: {
+			plugins: [autoprefixer()]
+		}
+	}),
 };
 
 export default config;

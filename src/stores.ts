7158a1e7bc404ea9147ee324d6e75313a336b46e
Shadofer <shadowrlrs@gmail.com>
2022-04-30 21:32:20 +0300

    Remove leftover properties.

--- src/stores.ts
+++ src/stores.ts
@@ -2,9 +2,5 @@
 // Shared variables for all kinds of files.
 // ******************** //
 
-// The 'shadow-animated' class sensitivities
-export const animationSensitivityX = 0.0025;
-export const animationSensitivityY = 0.005;
-
 // Global scrolling options for svelte-scrolling
 export const customScrollDuration = 750;

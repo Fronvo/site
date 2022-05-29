723a9ca31f8910b7bcddefbc3b4365a862521cec
Shadofer <shadowrlrs@gmail.com>
2022-05-29 19:43:58 +0300

    Seperate DownArrow component

---
+++ src/lib/svgs/DownArrow.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let width = '48px';
+    export let height = '48px';
+</script>
+
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    {width}
+    {height}
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    stroke-width={2}
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather-icon"><polyline points="6 9 12 15 18 9" /></svg
+>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,7 @@
 
 <script lang="ts">
     import Saos from 'saos';
+    import DownArrow from 'src/lib/svgs/DownArrow.svelte';
     import Github from 'src/lib/svgs/Github.svelte';
     import Wave from 'src/lib/svgs/Wave.svelte';
     import { customScrollDuration } from 'src/stores';
@@ -67,19 +68,7 @@
                 use:scrollTo={'fronvo-safe'}
                 in:fly={{ duration: 300, y: 10, delay: 1900, easing: cubicOut }}
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={'48px'}
-                    height={'48px'}
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width={2}
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="feather-icon"
-                    ><polyline points="6 9 12 15 18 9" /></svg
-                >
+                <DownArrow />
             </div>
         </div>
 

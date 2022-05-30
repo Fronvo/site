a9abdcfd6b49dac2b51b0db787785a07d7ed1df4
Shadofer <shadowrlrs@gmail.com>
2022-05-30 19:03:50 +0300

    Set global scrolling options in __layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,14 @@
 <script>
     import 'src/app.css';
+    import { setGlobalOptions } from 'svelte-scrolling';
+    import { sineInOut } from 'svelte/easing';
+
+    // svelte-scrolling globals
+    setGlobalOptions({
+        duration: 750,
+        easing: sineInOut,
+        offset: 0,
+    });
 </script>
 
 <!-- TODO: Theme wrapper -->

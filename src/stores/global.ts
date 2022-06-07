8b8421e2b3c990841c3da374e65269907a5f7724
Shadofer <shadowrlrs@gmail.com>
2022-06-07 18:12:19 +0300

    Update more paths

--- src/stores/global.ts
+++ src/stores/global.ts
@@ -2,10 +2,10 @@
 // Shared variables for all kinds of files.
 // ******************** //
 
+import type { ClientToServerEvents } from 'interfaces/socket/events/c2s';
+import type { ServerToClientEvents } from 'interfaces/socket/events/s2c';
 import { io, Socket } from 'socket.io-client';
 import binaryParser from 'socket.io-msgpack-parser';
-import type { ClientToServerEvents } from '../interfaces/socket/events/c2s';
-import type { ServerToClientEvents } from '../interfaces/socket/events/s2c';
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
     'wss://fronvosrv.herokuapp.com',

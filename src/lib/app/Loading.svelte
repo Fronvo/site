b1a37bb911a3dcc565f3627d5eab1564a285f29b
Shadofer <shadowrlrs@gmail.com>
2022-06-08 17:20:11 +0300

    Make `Loading` responsive

--- src/lib/app/Loading.svelte
+++ src/lib/app/Loading.svelte
@@ -7,7 +7,7 @@
 
 <Center>
     <div class="loading-container">
-        <h1>{text}</h1>
+        <h1 id="text">{text}</h1>
         <Shadow color="rgb(169, 64, 255)" size="48" duration="1s" />
     </div>
 </Center>
@@ -18,5 +18,21 @@
         flex-direction: column;
         justify-content: center;
         align-items: center;
+    }
+
+    #text {
+        font-size: 3rem;
+    }
+
+    @media screen and (max-width: 700px) {
+        #text {
+            font-size: 2.5rem;
+        }
+    }
+
+    @media screen and (max-width: 520px) {
+        #text {
+            font-size: 1.9rem;
+        }
     }
 </style>

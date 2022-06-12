a687b0feb6cb168be3d2ac0eeae4d1431c7d39f4
Shadofer <shadowrlrs@gmail.com>
2022-06-12 22:33:03 +0300

    Make `Logo` not selectable

--- src/lib/index/TopNav.svelte
+++ src/lib/index/TopNav.svelte
@@ -133,6 +133,12 @@
             padding: 0;
             display: block;
             cursor: default;
+            -webkit-touch-callout: none;
+            -webkit-user-select: none;
+            -khtml-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
         }
 
         .top-nav-container #logo:hover {

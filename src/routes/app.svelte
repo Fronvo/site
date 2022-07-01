0dfa66ff178fed35e1f06f059eb26b61d747a7cc
Shadofer <shadowrlrs@gmail.com>
2022-07-01 15:06:11 +0300

    Add `scrollbar` style

--- src/routes/app.svelte
+++ src/routes/app.svelte
@@ -3,3 +3,12 @@
 
     $showLayout = true;
 </script>
+
+<style>
+    /* Not needed on desktop with mobile width */
+    @media screen and (max-width: 720px) {
+        :global(::-webkit-scrollbar) {
+            width: 0px;
+        }
+    }
+</style>

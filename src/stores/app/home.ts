8862a0a1199da1a60af36a9270f1e05266dc2018
Shadofer <shadowrlrs@gmail.com>
2022-07-23 17:19:06 +0300

    Update `import` path

--- src/stores/app/home.ts
+++ src/stores/app/home.ts
@@ -2,7 +2,7 @@
 // Shared variables for the app home panel, after login.
 // ******************** //
 
-import type { HomeUpdates } from 'src/interfaces/app/home';
+import type { HomeUpdates } from 'interfaces/app/home';
 import { writable, type Writable } from 'svelte/store';
 
 export const homeUpdates: Writable<HomeUpdates[]> = writable();

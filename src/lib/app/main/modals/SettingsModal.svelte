12e6bdfc7810fa9894a1a4ebf402bc2e0409a3b2
Shadofer <shadowrlrs@gmail.com>
2022-08-07 19:22:00 +0300

    Modernise the `Settings` modal

--- src/lib/app/main/modals/SettingsModal.svelte
+++ src/lib/app/main/modals/SettingsModal.svelte
@@ -30,35 +30,47 @@
     }
 </script>
 
-<div class="header-container">
-    <Close />
-    <h1 id="header">Settings</h1>
-</div>
-
-<hr />
-
-<div class="options-container">
-    <div>
-        <button on:click={logout}>Logout</button>
+<div class="settings-container">
+    <div class="header-container">
+        <h1 id="header">Settings</h1>
     </div>
+
+    <hr />
+
+    <div class="data-container">
+        <div>
+            <h1>Exit your account</h1>
+            <button on:click={logout}>Logout</button>
+        </div>
+    </div>
+
+    <button id="close" on:click={() => ($modalVisible = false)}>Close</button>
 </div>
 
 <style>
     hr {
         width: 100px;
-        margin-bottom: 20px;
+    }
+
+    .settings-container {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+        align-items: center;
+        overflow-y: auto;
     }
 
     .header-container {
         display: flex;
+        width: 100%;
         justify-content: center;
-        align-items: center;
     }
 
     .header-container #header {
         font-size: 3rem;
         margin: 0;
-        padding-right: 40px;
+        padding-right: 5px;
         -webkit-touch-callout: none;
         -webkit-user-select: none;
         -khtml-user-select: none;
@@ -67,15 +79,26 @@
         user-select: none;
     }
 
-    .options-container div {
+    .data-container {
         display: flex;
+        flex-direction: column;
+        flex: 1;
+        justify-content: center;
+        width: 50%;
+        min-width: 500px;
+    }
+
+    .data-container div {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
         justify-content: center;
         align-items: center;
         margin-bottom: 25px;
         flex-wrap: wrap;
     }
 
-    .options-container div h1 {
+    .data-container div h1 {
         margin: 0;
         margin-right: 15px;
         font-size: 2.4rem;
@@ -87,20 +110,35 @@
         user-select: none;
     }
 
-    .options-container div button {
+    .data-container div button {
         font-size: 2.2rem;
+    }
+
+    #close {
+        font-size: 2.2rem;
+        margin-bottom: 15px;
     }
 
     @media screen and (max-width: 720px) {
         .header-container #header {
-            font-size: 2.7rem;
+            font-size: 2.4rem;
         }
 
-        .options-container div h1 {
+        .data-container {
+            width: 400px;
+            min-width: auto;
+        }
+
+        .data-container div h1 {
             font-size: 2.1rem;
         }
 
-        .options-container div button {
+        .data-container div button {
+            font-size: 1.8rem;
+            cursor: default;
+        }
+
+        #close {
             font-size: 1.8rem;
             cursor: default;
         }
@@ -108,15 +146,23 @@
 
     @media screen and (max-width: 520px) {
         .header-container #header {
-            font-size: 2.3rem;
+            font-size: 2rem;
         }
 
-        .options-container div h1 {
+        .data-container {
+            width: 300px;
+        }
+
+        .data-container div h1 {
             font-size: 1.8rem;
         }
 
-        .options-container div button {
-            font-size: 1.6rem;
+        .data-container div button {
+            font-size: 1.5rem;
+        }
+
+        #close {
+            font-size: 1.5rem;
         }
     }
 </style>

a04fe9293dff12a34cff14cd5e30bda7c1d80263
Shadofer <shadowrlrs@gmail.com>
2022-08-26 18:35:57 +0300

    Remove `PostCSS`

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-// Plugins
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-
-module.exports = {
-    plugins: [autoprefixer, process.env.NODE_ENV === 'production' && cssnano],
-};

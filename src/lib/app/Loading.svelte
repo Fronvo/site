e63bbd2832d52d5d15e4b125377c77e22e80bf18
Shadofer <shadowrlrs@gmail.com>
2022-10-19 15:41:31 +0300

    Update `Loading` color

--- src/lib/app/Loading.svelte
+++ src/lib/app/Loading.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { darkTheme } from 'stores/all';
     import { Shadow } from 'svelte-loading-spinners';
     import { fade } from 'svelte/transition';
     import Center from './Center.svelte';
@@ -9,7 +10,13 @@
 <Center absolute>
     <div class="loading-container" transition:fade>
         <h1 id="text">{text}</h1>
-        <Shadow color="rgb(180, 120, 255)" size="48" duration="1s" />
+        <Shadow
+            color={`${
+                $darkTheme ? 'rgb(180, 120, 255)' : 'rgb(150, 100, 250)'
+            }`}
+            size="48"
+            duration="1s"
+        />
     </div>
 </Center>
 
